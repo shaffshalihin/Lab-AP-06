544905141812701aaaed10204f0390534d417451
MIJOHETA <wizwoz344@gmail.com>
2024-09-05 12:05:22 +0800

    kumpul tugas 1

---
+++ H071241051/Praktikum Ke-1/soal no 4.py
@@ -0,0 +1,8 @@
+Suhu=int(input("masukan suhu dalam celsius = "))
+kelvin= Suhu + float(273.15)
+reamur= Suhu * 4 /5
+fahrenheit = (Suhu * 9 / 5)+32
+print('Hasil konversi dari suhu',Suhu,'derajat celsius ke kelvin adalah :' ,int(kelvin), ' K derajat')
+print('Hasil konversi dari suhu',Suhu,'derajat celsius ke reamur adalah :' ,int(reamur), ' Reamur derajat')
+print('Hasil konversi dari suhu',Suhu,'derajat celsius ke fahrenheit adalah :' ,int(fahrenheit), ' F derajat')
+
