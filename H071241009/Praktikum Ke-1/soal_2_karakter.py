fbd2770494cf215592c6423e567e46b012bef3d2
MahesaPutriLukman <mahesaputrilukman28@gmail.com>
2024-09-08 10:01:25 +0800

    Tugas praktikum lab 1 Mahesa Putri Lukman H071241009 (revisi)

---
+++ H071241009/Praktikum Ke-1/soal_2_karakter.py
@@ -0,0 +1,12 @@
+# Program Karakter dalam Kalimat
+
+print("## Program Karakter dalam Kalimat ##")
+print("===========================")
+print()
+
+karakter = input("Masukkan Karakter : ")
+kalimat = input("Masukkan Kalimat : ")
+
+cek_karakter = (karakter in kalimat) and f"{karakter} terdapat dalam \"{kalimat}\"" or f"{karakter} tidak terdapat dalam \"{kalimat}\""
+
+print(cek_karakter)
