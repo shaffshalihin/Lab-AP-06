feef71578f56295e735ebb066f15dd1f331affd4
makhluksatur <andialy2006@gmail.com>
2024-09-10 11:43:01 +0800

    tuprak 1

---
+++ H071241036/Praktikum-1/soal2.py
@@ -0,0 +1,10 @@
+karakter = input("Masukkan Karakter : ")
+kalimat = input("Masukkan Kalimat : ")
+
+output = karakter in kalimat
+
+pesan = ["tidak ditemukan dalam", "merupakan bagian dari"][output]
+
+hasil = f"{karakter} {pesan} '{kalimat}'"
+print(hasil)
+print(type(hasil))
