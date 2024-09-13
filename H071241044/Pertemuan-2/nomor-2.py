3c8aaae02729e13fa120bd998553e9b3629ab1ea
twelverakki <twelve.rakki@gmail>
2024-09-13 14:04:03 +0800

    pembaruan tuprak 2

---
+++ H071241044/Pertemuan-2/nomor-2.py
@@ -0,0 +1,15 @@
+usia		= int(input("Masukkan usia: "))
+keanggotaan = input("Apakah Anda anggota? (ya/tidak): ")
+
+if usia < 5:
+    harga_tiket = 0
+elif 5 <= usia <= 12:
+    harga_tiket = 50000
+elif 13 <= usia <= 59:
+    harga_tiket = 100000
+else:
+    harga_tiket = 70000
+
+harga_total = harga_tiket * 0.8 if keanggotaan == 'ya' else harga_tiket
+
+print(f"Harga tiket yang harus dibayar: Rp{int(harga_total)}")
