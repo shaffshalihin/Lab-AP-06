7ec5da3c55004765de11e19e78d2e7d909db3bbf
Pdddo <stard4c@gmail.com>
2024-09-24 12:15:00 +0800

    Kumpul tugas 3

---
+++ H071241007/Praktikum-3/3.soal4.py
@@ -0,0 +1,22 @@
+pecahan_uang = [100000, 50000, 20000, 10000, 5000, 2000, 1000]
+
+total_harga = int(input("Masukkan total harga: "))
+uang_diberikan = int(input("Masukkan jumlah uang yang diberikan: "))
+
+
+kembalian = uang_diberikan - total_harga
+
+if kembalian < 0:
+    print("uang tidak cukup")
+else:
+    print(f"Kembalian: {kembalian}")
+ 
+if kembalian > 0:
+    print("Pecahan uang kembalian:")
+    for pecahan in pecahan_uang:
+        jumlah_lembar = kembalian // pecahan
+        if jumlah_lembar > 0:
+            print(f"Pecahan {pecahan}: {jumlah_lembar} lembar")
+        kembalian = kembalian % pecahan
+else:
+    print("Tidak ada kembalian.")
