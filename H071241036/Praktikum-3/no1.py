576bd57712b402078bf76385dc589c865bbe8336
makhluksatur <andialy2006@gmail.com>
2024-10-15 11:39:11 +0800

    Praktikum-3

---
+++ H071241036/Praktikum-3/no1.py
@@ -0,0 +1,21 @@
+baris_belah_ketupat = int(input("Masukkan Jumlah Baris : "))
+
+for i in range(baris_belah_ketupat):
+    if i % 2 == 1:
+        continue
+    else:
+        print(' ' * (baris_belah_ketupat - i) + '* ' * (i + 1))
+
+if baris_belah_ketupat % 2 == 1:
+    for i in range(baris_belah_ketupat):
+        if i % 2 == 0:
+            continue
+        else:
+            print(' ' * (i + 2) + '* ' * (baris_belah_ketupat - i - 1))
+
+else:
+    for i in range(baris_belah_ketupat):
+        if i % 2 == 1:
+            continue
+        else:
+            print(' ' * (i + 1) + '* ' * (baris_belah_ketupat - i))
